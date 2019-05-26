d602d25d3352154bd599e8e95a0d70efc3dbb593
Evan Porter <evan.porter@dotman.ca>
2019-05-26 16:59:52 -0300

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Ball from "./Ball.svelte";
+
+  export let names = ["Susan", "Dave", "Carol"];
+</script>
+
+<style>
+  div {
+    background-color: #f08c2b;
+  }
+</style>
+
+<div>
+  {#each names as name}
+    <Ball {name} />
+  {/each}
+</div>
---
+++ src/Ball.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  div {
+    display: table;
+    width: 150px;
+    height: 150px;
+    background: white;
+    border: 8px solid #7c4726;
+    border-radius: 50%;
+    line-height: 40px;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  span {
+    display: table-cell;
+    vertical-align: middle;
+  }
+</style>
+
+<div>
+  <span>{name}</span>
+</div>
