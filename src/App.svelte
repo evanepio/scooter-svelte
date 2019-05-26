1f9d8a334e76d184bb54c98a48abb7b461ea9c45
Evan Porter <evan.porter@dotman.ca>
2019-05-26 17:59:01 -0300

    Add controls to the layout

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,40 @@
 </script>
 
 <style>
+  div {
+    display: flex;
+    flex-direction: row;
+  }
 
+  .controls {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .main {
+    flex-grow: 4;
+  }
+
+  button {
+    color: white;
+    display: block;
+  }
+  .go {
+    background-color: #a7c766;
+  }
+  .reset {
+    background-color: #e52a1d;
+  }
 </style>
 
-<BallField {names} />
+<div>
+  <section class="controls">
+    <button class="reset">Reset</button>
+    <button class="go">Go</button>
+  </section>
+  <section class="main">
+    <BallField {names} />
+  </section>
+
+</div>
