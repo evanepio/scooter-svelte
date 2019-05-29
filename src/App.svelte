160afa362dc4a2c5be3803cbd239b58fedd8d12a
Evan Porter <evan.porter@dotman.ca>
2019-05-28 22:13:14 -0300

    Move state handling to the store

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,22 @@
 <script>
+  import { entries } from "./stores/entries.js";
   import ControlPanel from "./ControlPanel.svelte";
   import BallField from "./BallField.svelte";
 
-  let entries = [];
-  let id = 0;
-
-  function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function oneInNChances(numChances) {
-    return getRandom(1, numChances) == 1;
-  }
-
   function handleGo() {
-    entries.forEach(entry => {
-      let nonEliminated = entries.filter(entry => !entry.eliminated).length;
-      if (!entry.eliminated) {
-        if (nonEliminated == 1) {
-          entry.winner = true;
-        } else {
-          entry.eliminated = oneInNChances(4);
-        }
-      }
-    });
-
-    if (entries.filter(entry => !entry.eliminated).length == 1) {
-      entries.forEach(entry => {
-        if (!entry.eliminated) {
-          entry.winner = true;
-        }
-      });
-    }
-
-    entries = entries;
+    entries.playRound();
   }
 
   function handleReset() {
-    entries = entries.map(entry => {
-      return { ...entry, eliminated: false, winner: false };
-    });
+    entries.reset();
   }
 
   function handleAddName(event) {
-    let entry = {
-      name: event.detail,
-      id: id++,
-      eliminated: false,
-      winner: false
-    };
-    entries = [...entries, entry];
+    entries.addEntry(event.detail);
   }
 
   function handleBallClick(event) {
-    entries = entries.filter(entry => event.detail.id != entry.id);
+    entries.removeEntry(event.detail);
   }
 </script>
 
@@ -78,6 +41,6 @@
       on:addname={handleAddName} />
   </section>
   <section class="main">
-    <BallField {entries} on:ballclicked={handleBallClick} />
+    <BallField entries={$entries} on:ballclicked={handleBallClick} />
   </section>
 </div>
---
+++ src/stores/entries.js
@@ -0,0 +1,71 @@
+import { writable } from "svelte/store";
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function oneInNChances(numChances) {
+  return getRandom(1, numChances) == 1;
+}
+
+let id = 0;
+
+const { subscribe, set, update } = writable([]);
+
+const addEntry = name => {
+  console.log(name);
+  update(entries => [
+    ...entries,
+    {
+      name,
+      id: id++,
+      eliminated: false,
+      winner: false
+    }
+  ]);
+};
+
+const removeEntry = entryToRemove =>
+  update(entries => entries.filter(entry => entryToRemove.id != entry.id));
+
+const reset = () =>
+  update(entries =>
+    entries.map(entry => {
+      entry.eliminated = false;
+      entry.winner = false;
+      return entry;
+    })
+  );
+
+const clear = () => set([]);
+
+const playRound = () =>
+  update(entries => {
+    entries.forEach(entry => {
+      let nonEliminated = entries.filter(entry => !entry.eliminated).length;
+      if (!entry.eliminated) {
+        if (nonEliminated == 1) {
+          entry.winner = true;
+        } else {
+          entry.eliminated = oneInNChances(4);
+        }
+      }
+    });
+    if (entries.filter(entry => !entry.eliminated).length == 1) {
+      entries.forEach(entry => {
+        if (!entry.eliminated) {
+          entry.winner = true;
+        }
+      });
+    }
+    return entries;
+  });
+
+export const entries = {
+  subscribe,
+  playRound,
+  addEntry,
+  removeEntry,
+  reset,
+  clear
+};
