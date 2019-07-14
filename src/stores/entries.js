9373fd2cde229ce109c6d8cf23733d2cca492b2c
Evan Porter <evan.porter@dotman.ca>
2019-07-13 22:08:58 -0300

    Make entires a IIFE, because why not

--- src/stores/entries.js
+++ src/stores/entries.js
@@ -2,67 +2,67 @@ import { writable } from "svelte/store";
 
 import { oneInNChances, shuffleList } from "../util/random";
 
-let id = 0;
+export default (function() {
+  let id = 0;
 
-const { subscribe, set, update } = writable([]);
+  const { subscribe, set, update } = writable([]);
 
-const addEntry = name =>
-  update(entries => [
-    ...entries,
-    {
-      name,
-      id: id++,
-      eliminated: false,
-      winner: false
-    }
-  ]);
-
-const removeEntry = entryToRemove =>
-  update(entries => entries.filter(entry => entryToRemove.id != entry.id));
-
-const reset = () =>
-  update(entries =>
-    entries.map(entry => {
-      entry.eliminated = false;
-      entry.winner = false;
-      return entry;
-    })
-  );
-
-const clear = () => set([]);
-
-const playRound = () =>
-  update(entries => {
-    entries.forEach(entry => {
-      let nonEliminated = entries.filter(entry => !entry.eliminated).length;
-      if (!entry.eliminated) {
-        if (nonEliminated == 1) {
-          entry.winner = true;
-        } else {
-          entry.eliminated = oneInNChances(4);
-        }
+  const addEntry = name =>
+    update(entries => [
+      ...entries,
+      {
+        name,
+        id: id++,
+        eliminated: false,
+        winner: false
       }
-    });
-    if (entries.filter(entry => !entry.eliminated).length == 1) {
+    ]);
+
+  const removeEntry = entryToRemove =>
+    update(entries => entries.filter(entry => entryToRemove.id != entry.id));
+
+  const reset = () =>
+    update(entries =>
+      entries.map(entry => {
+        entry.eliminated = false;
+        entry.winner = false;
+        return entry;
+      })
+    );
+
+  const clear = () => set([]);
+
+  const playRound = () =>
+    update(entries => {
       entries.forEach(entry => {
+        let nonEliminated = entries.filter(entry => !entry.eliminated).length;
         if (!entry.eliminated) {
-          entry.winner = true;
+          if (nonEliminated == 1) {
+            entry.winner = true;
+          } else {
+            entry.eliminated = oneInNChances(4);
+          }
         }
       });
-    }
-    return entries;
-  });
+      if (entries.filter(entry => !entry.eliminated).length == 1) {
+        entries.forEach(entry => {
+          if (!entry.eliminated) {
+            entry.winner = true;
+          }
+        });
+      }
+      return entries;
+    });
 
-const shuffle = () => update(shuffleList);
+  const shuffle = () => update(shuffleList);
 
-const entries = {
-  subscribe,
-  playRound,
-  addEntry,
-  removeEntry,
-  shuffle,
-  reset,
-  clear
-};
-
-export default entries;
+  return {
+    subscribe,
+    playRound,
+    addEntry,
+    removeEntry,
+    shuffle,
+    reset,
+    clear
+  };
+})();
